nvimpel/chipwhisperer

commit 8b7c62866f10dc0d18d10a852cfc705cbdf02308

Husky: workaround for https://github.com/newaetech/chipwhisperer/issues/559
Ultimately a more proper fix is needed in the FPGA reset mechanism, but
this works well. Includes new on-target testing to ensure the issue
doesn't re-appear.

software/chipwhisperer/capture/scopes/OpenADC.py
@@ -653,7 +653,11 @@ class OpenADC(util.DisableNewAttr, ChipWhispererCommonInterface):
         """
         if not self._is_husky:
             raise ValueError("For CW-Husky only.")
-        self.sc.reset_fpga()
+        self.fpga_reg_write('RESET', [1])
+        self.fpga_reg_write('RESET', [0])
+        # TODO-temporary: when Husky bitfiles are fixed, replace RESET writes above with:
+        #self.sc._setReset(True)
+        #self.sc._setReset(False)
         self.adc._clear_caches()
         self.sc._clear_caches()
         self.gain._clear_caches()
@@ -690,8 +694,12 @@ class OpenADC(util.DisableNewAttr, ChipWhispererCommonInterface):
         cwtype = self._getCWType()
         if cwtype in ["cwhusky", "cwhuskyplus"]:
             self.sc._is_husky = True
-        self.sc._setReset(True)
-        self.sc._setReset(False)
+            # TODO-temporary: when Husky bitfiles are fixed, RESET writes below won't be necessary, can use _setReset() instead
+            self.fpga_reg_write('RESET', [1])
+            self.fpga_reg_write('RESET', [0])
+        else:
+            self.sc._setReset(True)
+            self.sc._setReset(False)
 
         self.adc = TriggerSettings(self.sc)
         self.gain = GainSettings(self.sc, self.adc)

software/chipwhisperer/capture/scopes/_OpenADCInterface.py
@@ -418,15 +418,6 @@ class OpenADCInterface(util.DisableNewAttr):
         address = self._address_str2int(address)
         return self.msg_read(address, maxResp=num_bytes)
 
-    def reset_fpga(self):
-        """ Reset all FPGA resgiters to their defaults.
-        """
-        if not self._is_husky:
-            raise ValueError("For CW-Husky only.")
-        self.sendMessage(CODE_WRITE, "RESET", [1])
-        self.sendMessage(CODE_WRITE, "RESET", [0])
-
-
     def setSettings(self, state, validate=False):
         cmd = bytearray(1)
         cmd[0] = state
@@ -606,8 +597,9 @@ class OpenADCInterface(util.DisableNewAttr):
 
         self.flushInput()
 
-        #Reset... will automatically clear by the time we are done
+        #Reset... self-clearing on CW-lite/pro, but NOT on Husky family
         self._setReset(True)
+        self._setReset(False)
         self.flushInput()
 
         #Send ping

tests/test_husky_dev.py
@@ -101,7 +101,7 @@ cw.scope_logger.setLevel(cw.logging.ERROR) # don't want to see warnings when set
 
 # TODO: program FW?
 scope.XADC.user_reset() # reset max/min stats
-scope.sc.reset_fpga()
+scope.reset_fpga()
 scope.adc.clip_errors_disabled = True
 scope.adc.lo_gain_errors_disabled = True
 scope.clock.clkgen_freq = 10e6
@@ -191,6 +191,14 @@ testData = [
     (500,       0,          'internal', 20e6,       False,      1,      12, False,  1,      0,      1,      'slowreads'),
     ('max',     0,          'internal', 20e6,       False,      1,      12, False,  1,      0,      1,      'maxslowreads_SLOW'),
 ]
+
+
+testADCresetData = [
+    # freq_start    freq_stop   freq_step   adc_mul_start   adc_mul_stop    adc_mul_step    desc
+    (7.37e6,        8e6,        1e6,        1,              'max',          1,              'ADCreset'),
+    (7.37e6,        20e6,       1e6,        1,              'max',          1,              'ADCreset_SLOW'),
+]
+
 
 testADCsweep = [
     # samples   presamples  freq_start  freq_stop   freq_step   testmode    fastreads   adcmul  bit stream  segs    segcycs reps    desc
@@ -571,6 +579,78 @@ def test_internal_ramp(fulltest, samples, presamples, testmode, clock, fastreads
         assert errors == 0, "%d errors; First error: %d; scope.adc.errors: %s" % (errors, first_error, scope.adc.errors)
         assert scope.adc.errors == False
     scope.sc._fast_fifo_read_enable = True # return to default
+
+
+
+@pytest.mark.parametrize("freq_start, freq_stop, freq_step, adc_mul_start, adc_mul_stop, adc_mul_step, desc", testADCresetData)
+def test_adc_reset(fulltest, reps, freq_start, freq_stop, freq_step, adc_mul_start, adc_mul_stop, adc_mul_step, desc):
+    # The goal of this test is to ensure that when the FPGA is reset, which scope.con() does, everything works properly.
+    # We test this with a wide range of ADC clock frequencies.
+    # This is in response to this bug: https://github.com/newaetech/chipwhisperer/issues/559
+    if not fulltest and 'SLOW' in desc:
+        pytest.skip("use --fulltest to run")
+        return None
+    if not fulltest:
+        reps = 1 # reduce number of reps to speed up
+    fhits = []
+    if freq_stop == 'max':
+        freq_stop = MAXCLOCK
+    if adc_mul_stop == 'max':
+        use_max_adc_mul_stop = True
+    else:
+        use_max_adc_mul_stop = False
+    for clock in range(int(freq_start), int(freq_stop), int(freq_step)):
+        if use_max_adc_mul_stop:
+            adc_mul_stop = int(MAXCLOCK//clock)
+        if adc_mul_stop == 0:
+            adc_mul_stop = 1
+        for adcmul in range(adc_mul_start, adc_mul_stop+1, adc_mul_step):
+            if adcmul * clock > MAXCLOCK:
+                if verbose: print('skipping mul=%d, clock=%d' % (adcmul, clock))
+                next
+            if adcmul*clock in fhits:
+                if verbose: print('skipping (already done)')
+                next
+            fhits.append(adcmul*clock)
+            samples = MAXSAMPLES
+            adc_reset_test_setup(samples)
+            scope.clock.clkgen_freq = clock
+            scope.clock.adc_mul = adcmul
+            time.sleep(0.1)
+            assert scope.clock.pll.pll_locked == True
+            #assert abs(scope.clock.adc_freq - clock * adcmul) <= 10e6
+            if verbose: print('Running clock=%d, mul=%d, adc=%d' % (clock/1e6, adcmul, scope.clock.adc_freq/1e6))
+            for i in range(reps):
+                scope.sc.arm(False)
+                scope.arm()
+                scope.sc.triggerNow()
+                scope.sc.arm(False)
+                assert scope.capture() == False
+                raw = np.int64(scope.get_last_trace(True))
+                errors, first_error = check_ramp(raw, 'internal', 12, samples, 0)
+                assert errors == 0, "at ADC clock=%d MHz: %d errors; First error: %d; scope.adc.errors: %s" % (scope.clock.adc_freq/1e6, errors, first_error, scope.adc.errors)
+                assert scope.adc.errors == False, 'ADC errors at clock=%d MHz: %s' % (scope.clock.adc_freq/1e6, scope.adc.errors)
+
+    scope.adc.test_mode = False
+
+def adc_reset_test_setup(samples):
+    # reconnecting to the scope WITHOUT reprogramming the FPGA bitfile is an important part of this test:
+    # NOTE: manually reset -- not ideal since it's not what we want to test
+    #scope.sc._setReset(True)
+    #scope.sc._setReset(False)
+    # NOTE: for some reason, calling scope.dis()/con() here leads to strange errors elsewhere
+    #scope.dis()
+    #scope.con(hw_location=hw_loc)
+    scope = cw.scope(hw_location=hw_loc)
+    reset_setup(scope,target)
+    scope.adc.test_mode = True
+    scope.ADS4128.mode = 'normal'
+    scope.adc.samples = samples
+    scope.adc.presamples = 0
+    scope.adc.segments = 1
+    scope.adc.clip_errors_disabled = True
+    scope.adc.lo_gain_errors_disabled = True
+    scope.adc.bits_per_sample = 12
 
 
 

tests/test_husky_prod.py
@@ -72,7 +72,7 @@ verbose = False
 cw.scope_logger.setLevel(cw.logging.ERROR) # don't want to see warnings when setting clock past its specifications
 
 scope.XADC.user_reset() # reset max/min stats
-scope.sc.reset_fpga()
+scope.reset_fpga()
 scope.adc.clip_errors_disabled = True
 scope.adc.lo_gain_errors_disabled = True
 scope.clock.clkgen_freq = 10e6

tests/test_husky_stream.py
@@ -57,7 +57,7 @@ verbose = False
 cw.scope_logger.setLevel(cw.logging.ERROR) # don't want to see warnings when setting clock past its specifications
 
 # TODO: program FW?
-scope.sc.reset_fpga()
+scope.reset_fpga()
 scope.adc.clip_errors_disabled = True
 scope.adc.lo_gain_errors_disabled = True
 scope.clock.clkgen_freq = 10e6

commit 6c3f62e7cf0bb5adf656358937a1516bc5957c54

Fix for https://github.com/newaetech/chipwhisperer/issues/561
(inadvertently broken by 8b7c62866f10dc0d18d10a852cfc705cbdf02308)

software/chipwhisperer/capture/scopes/OpenADC.py
@@ -697,6 +697,7 @@ class OpenADC(util.DisableNewAttr, ChipWhispererCommonInterface):
             # TODO-temporary: when Husky bitfiles are fixed, RESET writes below won't be necessary, can use _setReset() instead
             self.fpga_reg_write('RESET', [1])
             self.fpga_reg_write('RESET', [0])
+            self.sc._setMaxSamples() # TODO: won't be needed when _setReset can be called
         else:
             self.sc._setReset(True)
             self.sc._setReset(False)

software/chipwhisperer/capture/scopes/_OpenADCInterface.py
@@ -438,15 +438,21 @@ class OpenADCInterface(util.DisableNewAttr):
         """
         if value:
             self.setSettings(self.settings() | SETTINGS_RESET, validate=False)
-            if self._is_husky:
-                self.hwMaxSamples = self.numMaxSamples()
-                self.hwMaxSegmentSamples = self.numMaxSegmentSamples()
-            else:
-                #Hack to adjust the hwMaxSamples since the number should be smaller than what is being returned
-                self.hwMaxSamples = self.numSamples() - 45
-            self.setNumSamples(self.hwMaxSamples)
+            self._setMaxSamples()
         else:
             self.setSettings(self.settings() & ~SETTINGS_RESET)
+
+    def _setMaxSamples(self):
+        """Note: this is only intended to be called when connecting. If used outside of this, cached register values may be not reflect reality.
+        """
+        if self._is_husky:
+            self.hwMaxSamples = self.numMaxSamples()
+            self.hwMaxSegmentSamples = self.numMaxSegmentSamples()
+        else:
+            #Hack to adjust the hwMaxSamples since the number should be smaller than what is being returned
+            self.hwMaxSamples = self.numSamples() - 45
+        self.setNumSamples(self.hwMaxSamples)
+
 
     def triggerNow(self):
         initial = self.settings(True)